0543843826ce15910af577a75db446eba644603e
Colin Quirk <cquirk@uchicago.edu>
2019-11-12 09:43:15 -0600

    prevent crash on wrong keypress

--- keypress_handler.R
+++ keypress_handler.R
@@ -1,10 +1,12 @@
 keypress_handler <- 
   '$(document).on("shiny:value", function(e) {
-     if(e.name == "trace_plot") {
+     if (e.name == "trace_plot") {
        Shiny.setInputValue("keypress", 0);
      }
    });
             
    $(document).on("keypress", function (e) {
-     Shiny.setInputValue("keypress", e.key);
+     if (["1", "2", "3"].includes(e.key)) {
+       Shiny.setInputValue("keypress", e.key);
+     }
    });'
